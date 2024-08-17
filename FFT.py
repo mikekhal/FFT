13e4d8179827d48c96dbc2f48e9745b17d780718
mikekhal <mike.khaluk@gmail.com>
2024-08-17 17:48:27 +0100

    Add files via upload

--- FFT.py
+++ FFT.py
@@ -5,79 +5,74 @@ Created on Thu Aug 15 06:09:50 2024
 @author: mike_
 """
 import numpy as np
+import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 
-# Parameters
-opd = np.linspace(0, 10, 1000)  # Optical Path Difference (micrometers)
-delta_opd = opd[1] - opd[0]     # Sampling interval
+# Read the audio file
+sample_rate, data = wav.read('Bad_Sample_1 (1).wav')
 
-# Hydrogen Spectrum Simulation
-def hydrogen_spectrum(opd, wavelengths, amplitudes):
-    interferogram = np.zeros_like(opd)
-    for wavelength, amplitude in zip(wavelengths, amplitudes):
-        frequency = 1 / wavelength
-        interferogram += amplitude * np.cos(2 * np.pi * frequency * opd)
-    return interferogram
+# FFT the data
+fft_data = np.fft.fft(data)
+# Get an array for the frequencies in correct units (Hz)
+freqs = np.fft.fftfreq(len(data), d=1./sample_rate)
 
-# White Light Spectrum Simulation
-def white_light_spectrum(opd, mean_wavelength, sigma):
-    frequencies = np.fft.fftfreq(len(opd), delta_opd)
-    gaussian_spectrum = np.exp(-0.5 * (frequencies - 1 / mean_wavelength)**2 / (sigma**2))
-    interferogram = np.fft.ifft(gaussian_spectrum)
-    return np.real(interferogram)
+# Print frequencies and Fourier coefficients
+print('Frequencies: ', freqs)
+print('Fourier Coefficients: ', fft_data)
 
-# Hydrogen Spectral Lines (wavelengths in micrometers)
-hydrogen_wavelengths = [0.4861, 0.6563, 0.4340]  # H-beta, H-alpha, H-gamma
-hydrogen_amplitudes = [1.0, 0.8, 0.6]  # Arbitrary units
-hydrogen_interferogram = hydrogen_spectrum(opd, hydrogen_wavelengths, hydrogen_amplitudes)
-
-# White Light Spectrum
-mean_wavelength = 0.55  # Central wavelength in micrometers
-sigma = 0.1            # Standard deviation of the Gaussian (broadness of the spectrum)
-white_light_interferogram = white_light_spectrum(opd, mean_wavelength, sigma)
-
-# Plot Hydrogen Interferogram
-plt.figure(figsize=(12, 6))
-plt.plot(opd, hydrogen_interferogram)
-plt.xlabel('Optical Path Difference (micrometers)')
-plt.ylabel('Intensity (arbitrary units)')
-plt.title('Simulated Hydrogen Spectral Lamp Interferogram')
-plt.grid(True)
+# Plot the frequency values as a function of array index
+plt.plot(freqs)
+plt.xlabel('Array index')
+plt.ylabel('Frequency [Hz]')
 plt.show()
 
-# Plot White Light Interferogram
-plt.figure(figsize=(12, 6))
-plt.plot(opd, white_light_interferogram)
-plt.xlabel('Optical Path Difference (micrometers)')
-plt.ylabel('Intensity (arbitrary units)')
-plt.title('Simulated White Light Interferogram')
-plt.grid(True)
+# Plot the power spectral density
+PSD_audio = np.fft.fftshift(np.real(fft_data * np.conj(fft_data)))
+plt.plot(np.fft.fftshift(freqs), PSD_audio)
+plt.yscale('log')
+plt.ylabel('Power Spectral Density')
+plt.xlabel('Frequency [Hz]')
 plt.show()
 
-# Perform FFT on Hydrogen Interferogram
-fft_result_hydrogen = np.fft.fft(hydrogen_interferogram)
-freqs_hydrogen = np.fft.fftfreq(len(hydrogen_interferogram), delta_opd)
-magnitude_hydrogen = np.abs(fft_result_hydrogen)
+# Set thresholds for filtering
+lower_threshold = -2000
+upper_threshold = 2000
 
-# Perform FFT on White Light Interferogram
-fft_result_white_light = np.fft.fft(white_light_interferogram)
-freqs_white_light = np.fft.fftfreq(len(white_light_interferogram), delta_opd)
-magnitude_white_light = np.abs(fft_result_white_light)
+# Find indices where freqs satisfy the condition
+indices_to_zero = np.where((np.abs(freqs) < lower_threshold) | (np.abs(freqs) > upper_threshold))
 
-# Plot Frequency Spectrum of Hydrogen
-plt.figure(figsize=(12, 6))
-plt.plot(freqs_hydrogen[:len(freqs_hydrogen)//2], magnitude_hydrogen[:len(magnitude_hydrogen)//2])
-plt.xlabel('Frequency (cycles per micrometer)')
-plt.ylabel('Magnitude')
-plt.title('Frequency Spectrum of Hydrogen Spectral Lamp')
-plt.grid(True)
+# Copy FFT data and zero out the frequencies outside the thresholds
+fft_data_clean = np.copy(fft_data)
+fft_data_clean[indices_to_zero] = 0
+
+# Plot the filtered power spectral density
+plt.plot(np.fft.fftshift(freqs), np.fft.fftshift(np.real(fft_data_clean * np.conj(fft_data_clean))))
+plt.yscale('log')
+plt.ylabel('Power Spectral Density')
+plt.xlabel('Frequency [Hz]')
 plt.show()
 
-# Plot Frequency Spectrum of White Light
-plt.figure(figsize=(12, 6))
-plt.plot(freqs_white_light[:len(freqs_white_light)//2], magnitude_white_light[:len(magnitude_white_light)//2])
-plt.xlabel('Frequency (cycles per micrometer)')
-plt.ylabel('Magnitude')
-plt.title('Frequency Spectrum of White Light')
-plt.grid(True)
+# Plot the zoomed-in power spectral density
+plt.plot(np.fft.fftshift(freqs), np.fft.fftshift(np.real(fft_data_clean * np.conj(fft_data_clean))))
+plt.xlim(0, upper_threshold)
+plt.yscale('log')
+plt.ylabel('Power Spectral Density')
+plt.xlabel('Frequency [Hz]')
 plt.show()
+
+# Inverse FFT to get back to the time domain
+filtered_data = np.fft.ifft(fft_data_clean)
+filtered_data = np.real(filtered_data)
+
+# Amplify the audio
+gain = 1000.0  # Adjust this value to increase or decrease the amplification
+amplified_data = filtered_data * gain
+
+# Normalize the amplified data to avoid clipping
+amplified_data = np.int16(amplified_data / np.max(np.abs(amplified_data)) * 32767)
+
+# Save the amplified and filtered audio to a new WAV file
+wav.write('amplified_filtered_output.wav', sample_rate, amplified_data)
+
+print("Amplified and filtered audio saved as 'amplified_filtered_output.wav'.")
+
